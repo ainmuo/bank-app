bb215c8ea07b379322401605e7356693fae60a1f
ainmuo <153458884+ainmuo@users.noreply.github.com>
2024-10-27 15:48:13 +0100

    Add files via upload

---
+++ bank-app/customer-service/src/main/java/ma/formations/msa/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package ma.formations.msa.customerservice.repository;
+
+import ma.formations.msa.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
